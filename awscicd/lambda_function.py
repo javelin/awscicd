37be1ea71d904e8daacf94177e9ef9d6fc012a39
Mark Documento <doc.javelin@gmail.com>
2019-10-13 16:34:30 +0800

    Test revision 1.

--- awscicd/lambda_function.py
+++ awscicd/lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda CI/CD!')
+        'body': json.dumps('Hello from Lambda CI/CD 1!')
     }
 
